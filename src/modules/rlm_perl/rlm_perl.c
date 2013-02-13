5f26da63c7d659df4045c35116ae8bf1a29c66f5
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2013-02-13 17:26:21 -0500

    Fix for new dictionary API

--- src/modules/rlm_perl/rlm_perl.c
+++ src/modules/rlm_perl/rlm_perl.c
@@ -567,7 +567,7 @@ static void perl_store_vps(VALUE_PAIR *vp, HV *rad_hv)
 		 *	<attribute>:<tag>, others just use the normal attribute
 		 *	name as the key.
 		 */
-		if (nvp->flags.has_tag && (nvp->tag != 0)) {
+		if (nvp->da->flags.has_tag && (nvp->tag != 0)) {
 			snprintf(namebuf, sizeof(namebuf), "%s:%d",
 			         nvp->da->name, nvp->tag);
 			name = namebuf;
@@ -579,7 +579,7 @@ static void perl_store_vps(VALUE_PAIR *vp, HV *rad_hv)
 		 *	Create a new list with all the attributes like this one
 		 *	which are in the same tag group.
 		 */
-		vpa = paircopy2(nvp, nvp->da->attribute, nvp->da->vendor, nvp->tag);
+		vpa = paircopy2(nvp, nvp->da->attr, nvp->da->vendor, nvp->tag);
 
 		/*
 		 *	Attribute has multiple values
@@ -609,7 +609,7 @@ static void perl_store_vps(VALUE_PAIR *vp, HV *rad_hv)
 		 *	Finally remove all the VPs we processed from our copy
 		 *	of the list.
 		 */
-		pairdelete(&nvp, nvp->da->attribute, nvp->da->vendor, nvp->tag);
+		pairdelete(&nvp, nvp->da->attr, nvp->da->vendor, nvp->tag);
 	}
 
 	pairfree(&nvp);		/* shouldn't be necessary, but hey... */
