627aa3fd0d5eaa31c0c194273b921eff656a738b
Alan T. DeKok <aland@freeradius.org>
2013-02-13 16:26:36 -0500

    Fix for new dictionary API

--- src/modules/rlm_realm/rlm_realm.c
+++ src/modules/rlm_realm/rlm_realm.c
@@ -181,7 +181,7 @@ static int check_for_realm(void *instance, REQUEST *request, REALM **returnrealm
 		 *	doesn't exist.
 		 *
 		 */
-		if (request->username->attribute != PW_STRIPPED_USER_NAME) {
+		if (request->username->da->attr != PW_STRIPPED_USER_NAME) {
 			vp = radius_paircreate(request, &request->packet->vps,
 					       PW_STRIPPED_USER_NAME, 0,
 					       PW_TYPE_STRING);
